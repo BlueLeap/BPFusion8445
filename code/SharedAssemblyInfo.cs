c8cc36ae0d9cd7d25033099debc0d2cf515980f1
sree112211 <141914844+sree112211@users.noreply.github.com>
2023-08-30 03:22:39 +0530

    Add files via upload

---
+++ code/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BlueLeap")]
+[assembly: AssemblyProduct("BlueLeap CTI Mediabar")]
+[assembly: AssemblyCopyright("Copyright © 2012 BlueLeap Corporation")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
