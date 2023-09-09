fbf88e54c0a0624d7e533e035018b8a38727bcb0
sree112211 <141914844+sree112211@users.noreply.github.com>
2023-09-10 04:01:15 +0530

    Update liveWinTelephonyAgent.js

--- toolbar/js/liveWinTelephonyAgent.js
+++ toolbar/js/liveWinTelephonyAgent.js
@@ -2965,7 +2965,7 @@
                     }
                     
                     if (agentId) {
-                        retrieveUniqueAgentId(agentId);
+                        //retrieveUniqueAgentId(agentId);
                     }
                  }
             });
